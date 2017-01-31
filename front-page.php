05eb559c8490b0d1f30693a91520bc619e317f0e
Nathan Conley <nateconley123@gmail.com>
2017-01-30 22:12:02 -0800

    Building dates pages and dates section on front page

--- front-page.php
+++ front-page.php
@@ -4,10 +4,64 @@
  */
 get_header(); ?>
 
-<div class="dates">
+<div class="dates-section">
 	<h3>dates</h3>
 	<div class="side">
-		
+
+		<?php
+
+		$midnight = strtotime( '00:00:00 yesterday' );
+
+		$args = array(
+			'post_type' 	=> 'shows',
+			'orderby' 		=> 'meta_value_num',
+			'meta_key' 		=> 'jldavis-show-date',
+			'order' 		=> 'ASC',
+			'meta_query' 	=> array(
+				array(
+					'key' 		=> 'jldavis-show-date',
+					'compare' 	=> '>=',
+					'value' 	=> $midnight,
+					'type' 		=> 'NUMERIC',
+				),
+			),
+		);
+
+		$query = new WP_Query( $args );
+
+		$format = '<div class="date">
+			<a href="%1$s">
+				<p>%2$s<span class="city"> %3$s </span>%4$s</p>
+			</a>
+		</div>';
+
+		if ( $query->have_posts() ) {
+			while ( $query->have_posts() ) {
+				$query->the_post();
+				$post_id = get_the_id();
+
+				$date = get_post_meta( $post_id, 'jldavis-show-date', true );
+				$date = date("n.j.y", $date);
+				$time = get_post_meta( $post_id, 'jldavis-show-time', true );
+				$city = get_post_meta( $post_id, 'jldavis-show-city', true );
+				$venue = get_post_meta( $post_id, 'jldavis-show-venue', true );
+
+				printf( $format,
+					get_post_permalink( $post_id ),
+					$venue,
+					$city,
+					$date
+				);
+			}
+		} else {
+			echo '<h2 class="not-found">No Dates Found</h2>'; 
+		}
+
+		wp_reset_postdata();
+
+		?>
+
+		<a href="<?php echo home_url(); ?>/dates" class="read-more">More Dates >></a>
 	</div>
 	<svg class="triangle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 8">
 		<path
