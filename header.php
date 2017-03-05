6c26834bae8855bfbbc28351be103de7a8080292
Nathan Conley <nateconley123@gmail.com>
2017-03-05 14:15:04 -0800

    Adding title to head

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 ?>
 <!doctype html>
 <head>
+	<title><?php bloginfo( 'name' ); ?></title>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
